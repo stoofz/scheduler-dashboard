20c79a230186c4485514d06c7a93cb8d0dd1574d
Anthony Chambers <ajfchambers@gmail.com>
2023-07-12 10:48:40 -0400

    Add binding

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -30,16 +30,26 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+   }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
+    /*
     const panels = data.map(panel => (
       <Panel
         key={panel.id}
@@ -48,6 +58,20 @@ class Dashboard extends Component {
         value={panel.value}
       />
     ));
+*/
+      const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+        .map(panel => (
+          <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)}
+          />
+      ));
+
+
+
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
